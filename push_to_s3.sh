df6eaa72576d84c2cc29ba2d652517967f8f1fe7
Michael Stepner <stepner@mit.edu>
2016-05-04 19:39:10 -0400

    Push files to S3 as public
    Otherwise people can't download them

--- push_to_s3.sh
+++ push_to_s3.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 export AWS_CONFIG_FILE=".aws_config"
-aws s3 sync geo_zip_files/ s3://files.michaelstepner.com --exclude .DS_Store
-aws s3 sync map_shapefiles/ s3://files.michaelstepner.com --exclude "*" --include "demo_maptile.*"
+aws s3 sync geo_zip_files/ s3://files.michaelstepner.com --acl public-read --exclude .DS_Store
+aws s3 sync map_shapefiles/ s3://files.michaelstepner.com --acl public-read --exclude "*" --include "demo_maptile.*"
