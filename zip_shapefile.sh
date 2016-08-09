d9cddb7f10c46bb1fa3b1f67dc81b2afdd15f980
Michael Stepner <stepner@mit.edu>
2016-08-08 23:37:06 -0400

    Change zip_shapefile script to use new ./build directories

--- zip_shapefile.sh
+++ zip_shapefile.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-cd map_shapefiles
-zip -FS ../geo_zip_files/geo_$1.zip $1_*.dta $1_*.ado $1_*.smcl
+cd build/geo_templates/$1
+zip ../../../geo_zip_files/geo_$1.zip $1_*.dta $1_*.ado $1_*.smcl
