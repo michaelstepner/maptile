481dac12780953718921e53709071a84d778249b
Michael Stepner <stepner@mit.edu>
2016-08-12 13:31:18 -0400

    Automatically zip creation files when creating zips

--- zip_shapefile.sh
+++ zip_shapefile.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
+
+echo "Zipping $1 template..."
 cd build/geo_templates/$1
-zip ../../../geo_zip_files/geo_$1.zip $1_*.dta $1_*.ado $1_*.smcl
+zip -FS ../../../geo_zip_files/geo_$1.zip $1_*.dta $1_*.ado $1_*.smcl
+
+echo "Zipping $1 creation files..."
+cd ../../archive
+cd $(ls -d */ | grep $1 | tail -n 1)  # find most recent archive
+zip -FS -r ../../../geo_zip_files/geo_$1_creation.zip *
