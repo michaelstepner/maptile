4535fcc1500643b22c59d628fcd43477f8ccb7ae
Michael Stepner <stepner@mit.edu>
2017-01-03 17:16:00 -0500

    Push images of maptile geo templates when pushing to S3

--- push_to_s3.sh
+++ push_to_s3.sh
@@ -2,3 +2,8 @@
 export AWS_CONFIG_FILE=".aws_config"
 aws s3 sync geo_zip_files/ s3://files.michaelstepner.com --acl public-read --exclude .DS_Store
 aws s3 sync build/geo_templates/demo/ s3://files.michaelstepner.com --acl public-read --exclude "*" --include "demo_maptile.*"
+
+for d in $(ls -d build/tests/*/)
+do
+  aws s3 sync $d s3://files.michaelstepner.com/geo_img --acl public-read --exclude "*" --include "*_noopt.png"
+done
